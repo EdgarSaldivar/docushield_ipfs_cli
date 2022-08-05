eacee75bc5ced1968d5841180216c8628dd7a58d
EdgarSaldivar <edgarls93@hotmail.com>
2022-08-04 17:35:56 -0700

    Update ipfs-docushield.sh

--- ipfs-docushield.sh
+++ ipfs-docushield.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install curl gnupg ca-certificates lsb-release
+sudo apt-get -y install curl gnupg ca-certificates lsb-release
 sudo apt-get update
-sudo apt-get install docker docker-compose
+sudo apt-get -y install docker docker-compose
 wget https://raw.githubusercontent.com/EdgarSaldivar/docushield_ipfs_cli/main/docker-compose.yaml 
 sudo docker-compose up -d
 /bin/sh -c "echo '/key/swarm/psk/1.0.0/\n/base16/\n52851b6e315ab5dadfe2fb70a7a22ff5f71b37a1c20b52760ce43a1ecbf3fdf6' > /mnt/storage/docushield/data/ipfs/swarm.key"
